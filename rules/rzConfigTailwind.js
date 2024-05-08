01c2e2facf70081b009b80e1dc4ad2891a73eb6b
Runkai Zhang <rzhang04@proton.me>
2024-05-08 02:18:42 -0700

    Fix issue

--- rules/rzConfigTailwind.js
+++ rules/rzConfigTailwind.js
@@ -3,10 +3,12 @@ import { FlatCompat } from '@eslint/eslintrc'
 const compat = new FlatCompat()
 
 const rzConfigTailwind = [
-    ...compat.config({
+    {
         name: 'runkai/tailwind',
-        extends: ['plugin:tailwindcss/recommended']
-    })
+        ...compat.config({
+            extends: ['plugin:tailwindcss/recommended']
+        })
+    }
 ]
 
 export default rzConfigTailwind
